e82e0f43429017e927ea81c9c05d9cec21bdb8a0
Kirill <apatski94@gmail.com>
2024-10-04 00:06:26 +0300

    feat: add Link to changeMasterKey and addMasterKey

--- src/screens/settings/settings.tsx
+++ src/screens/settings/settings.tsx
@@ -1,31 +1,46 @@
-import { Header } from '@/src/components/header/header'
-import { Text, View } from '@/src/components/Themed'
-import { Link } from 'expo-router'
-import { FC, memo } from 'react'
-import { TouchableOpacity } from 'react-native'
-import { styles } from './style'
+import {Header} from '@/src/components/header/header'
+import {Text, View} from '@/src/components/Themed'
+import {Link} from 'expo-router'
+import {FC, memo} from 'react'
+import {TouchableOpacity} from 'react-native'
+import {styles} from './style'
+import {useApp} from "@src/stores/app/app.store";
 
 export const SettingsScreen: FC = memo(() => {
-	return (
-		<View style={styles.container}>
-			<Header title='Настройки' />
-			<View style={styles.listBox}>
-				<Link href={'/aboutApp'} asChild>
-					<TouchableOpacity style={styles.button}>
-						<Text style={styles.text}>О приложении</Text>
-					</TouchableOpacity>
-				</Link>
+  const isMasterKey = useApp.use.state().isMasterKey
+  return (
+    <View style={styles.container}>
+      <Header title='Настройки'/>
+      <View style={styles.listBox}>
+        <Link href={'/aboutApp'} asChild>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.text}>О приложении</Text>
+          </TouchableOpacity>
+        </Link>
 
-				<TouchableOpacity style={styles.button}>
-					<Text style={styles.text}>Изменить мастер ключ</Text>
-				</TouchableOpacity>
+        {isMasterKey ? (
+          <Link href={'/changeMasterKey'} asChild>
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.text}>Изменить мастер ключ</Text>
+            </TouchableOpacity>
+          </Link>
+          )
+          : (
+            <Link href={'/addMasterKey'} asChild>
+              <TouchableOpacity style={styles.button}>
+                <Text style={styles.text}>Добавить мастер ключ</Text>
+              </TouchableOpacity>
+            </Link>
+          )
+        }
 
-				<Link href={'/clearSecretsModal'} asChild>
-					<TouchableOpacity style={styles.button}>
-						<Text style={styles.text}>Удалить все секреты</Text>
-					</TouchableOpacity>
-				</Link>
-			</View>
-		</View>
-	)
+
+        <Link href={'/clearSecretsModal'} asChild>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.text}>Удалить все секреты</Text>
+          </TouchableOpacity>
+        </Link>
+      </View>
+    </View>
+  )
 })
